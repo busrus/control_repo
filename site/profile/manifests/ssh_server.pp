467f5b5d8aa6ee5f00bd91ee6325be90574457d6
busrus <126582008+busrus@users.noreply.github.com>
2023-04-16 19:49:49 -0400

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
     ensure => present,
     user   => 'root',
     type   => 'ssh-rsa',
-    key    => '',
+    key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCujn+VsPp0JbRTLXxyAWJwhqs6NFCFfhXEyU0/vV2ONgfOhi2v3WRN7avddzOYUyr+iGoV2igKjUdVtd5L8Skmawyh5n3UBlUB4oGPKlHrsQ7EqNmu4b+VPC/rPrf4lMwNYJ5cNtwg2up65Nq+2D0aFUUT+80ADqGuebrCg3MEqVEXMV5kXiwdtGfrnA8+pNtoKNxv8kGB0mDUwBcesIZGw9zW0bR8W/F40kv0Bo51zwZrkajQOUbZ7rgg9FR7GDLdw/wrrlMcwPi/9cZghsD9yTrl6Fp4M9CbIdZ4vBA7ie0dY6nF1/QkMeEN2m0zkCsJyQh88HKis8bLMeJbjgSX',
   }  
 }
