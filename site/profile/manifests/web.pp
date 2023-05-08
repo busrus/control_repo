d19f8c63ef8469f2dcf8cd8d1979638bfe42dc85
Russell Henry <rusbus@pwcca.dome>
2023-05-08 10:03:50 -0400

    adding Hello World message to /var/www/index.html

--- site/profile/manifests/web.pp
+++ site/profile/manifests/web.pp
@@ -1,3 +1,10 @@
 class profile::web {
   include nginx
+
+  file { 'index.html':
+    path    => '/var/www/index.html',
+    ensure  => file,
+    content => 'Hello World!',
+    owner   => 'root',
+    mode    =>  '0655',
  }
